2e2c9d5ae8ea378b94cf5ec97baa6b613720a0ad
Iann Voltz <iann.voltz@telecom-paristech.fr>
2018-12-01 14:37:28 +0100

    Adds the team manager

---
+++ LD43/Assets/Scripts/Gameplay/Team/TeamManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+public interface TeamManagerInterface
+{
+    void AddCharacter (CharacterModel newCharacter);
+    void RemoveCharacter (int characterId);
+    CharacterModel GetCharacter (int characterId);
+}
+
+public class TeamManager : TeamManagerInterface
+{
+
+    private Dictionary<int, CharacterModel> m_Characters;
+
+    public TeamManager()
+    {
+        m_Characters = new Dictionary<int, CharacterModel> ();
+    }
+
+    public void AddCharacter (CharacterModel newCharacter)
+    {
+        m_Characters.Add (newCharacter.GetId (), newCharacter);
+    }
+
+    public void RemoveCharacter(int characterId)
+    {
+        m_Characters.Remove (characterId);
+    }
+
+    public CharacterModel GetCharacter (int characterId)
+    {
+        CharacterModel temp;
+        if (m_Characters.TryGetValue (characterId, out temp))
+        {
+            return temp;
+        }
+
+        return null;
+    }
+}
+
+public class TeamManagerProxy : UniqueProxy<TeamManagerInterface>
+{
+
+}
